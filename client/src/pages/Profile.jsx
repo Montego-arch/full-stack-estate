16ec9afcb354cbd74c3308614bd3671efc5e68d5
Montego-arch <mmanuelmiles@gmail.com>
2023-11-04 09:21:40 +0100

    complete listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -12,6 +12,7 @@ import {
   signOutUserStart,
  } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -152,8 +153,13 @@ export default function Profile() {
         <input type="text" placeholder="username" defaultValue={currentUser.username} id='username' class="border p-3 rounded-lg" onChange={handleChange}/>
         <input type="text" placeholder="email" defaultValue={currentUser.email} id='email' class="border p-3 rounded-lg" onChange={handleChange}/>
         <input type="password" placeholder="password" id='password' class="border p-3 rounded-lg" onChange={handleChange}/>
-        <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase
-        hover:opacity-95 disabled:op80'>{ loading ? 'Loading...' : 'Update'}</button>
+        <button disabled={loading} 
+        className='bg-slate-700 text-white rounded-lg p-3 uppercase
+        hover:opacity-95 disabled:op80'>
+          { loading ? 'Loading...' : 'Update'}
+        </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase
+        text-center hover:opacity-95' to={"/create-listing"}>Create Listing</Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>
