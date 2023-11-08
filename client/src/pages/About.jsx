ddef898c2d1d3531e76c2dbc9219da70b08361ec
Montego-arch <mmanuelmiles@gmail.com>
2023-11-08 16:40:56 +0100

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Paradise Estate</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu facilisis sapien. 
+        Morbi sit amet accumsan risus. Integer dui tellus, pretium ut est ac, 
+        sodales dictum elit. Cras tellus lacus, pulvinar a rhoncus vitae, 
+        ornare quis leo. Nulla ac metus sed libero aliquam elementum non non sapien. 
+        Maecenas quam lacus, iaculis id purus suscipit, porttitor mollis ante. 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer leo dolor, 
+        dignissim sit amet eleifend sed, faucibus sit amet metus. Nam in enim dui.</p>
+      <p className='mb-4 text-slate-700'>Nullam eleifend varius ultrices. Maecenas luctus at libero et varius. 
+        Suspendisse porta, sem eget pharetra tincidunt, urna ipsum interdum lectus, 
+        scelerisque suscipit nunc nulla a dui. Aenean vel tempus diam, eu mattis ex. 
+        Maecenas tincidunt sit amet enim ut faucibus. 
+        Morbi tincidunt, erat sed condimentum ullamcorper, nibh urna porttitor metus,
+        sed ultricies sem elit a augue. Aenean faucibus mi ac viverra lacinia.</p>
+      <p className='mb-4 text-slate-700'>Sed fringilla erat eget dignissim varius. Suspendisse in aliquet dui.
+        In in eleifend enim. Fusce eget enim ut tellus placerat tincidunt 
+        quis eu elit. Quisque ac sapien felis. Suspendisse vel ultricies justo. 
+        Aliquam mollis leo ligula, semper tincidunt nisi placerat vel. 
+        Sed vel placerat eros. Curabitur consectetur, ante ac congue consequat, 
+        risus sapien commodo orci, non viverra mi mi rutrum eros. 
+        Sed blandit, nibh ut porta fermentum, diam ipsum tincidunt enim.</p>
+    </div>
   )
 }
